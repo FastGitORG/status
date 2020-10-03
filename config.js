ff27ec9cb4be4c79e51713e167763dd57a7a819a
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-10-03 21:52:16 +0800

    update 03/10/2020

--- config.js
+++ config.js
@@ -1,27 +1,17 @@
-// 配置
 window.Config = {
 
-  // 站点名
-  SiteName: 'Status - FastGit',
+  SiteName: 'FastGit Status',
 
-  // 站点链接
   SiteUrl: '/',
 
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
     'ur919009-48563b164440b23c5d62bbf0'
   ],
 
-  // 是否显示监测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
-  // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
 
-  // 导航栏菜单
   Navi: [
     {
       text: 'Home',
